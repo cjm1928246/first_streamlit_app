6e3b725b51bf810276c894e04f65ecdc7f7847e1
cjm1928246 <111906865+cjm1928246@users.noreply.github.com>
2022-08-23 14:28:55 -0400

    Update streamlit_app.py
    Replaced content with lesson text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title('How well will Streamlit UI work?')
-streamlit.header('We\'ll find out!')
-streamlit.text('Hello World!')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
