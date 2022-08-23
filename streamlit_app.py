6f2119847e5ecf56088701a1e395b51eb8920aff
cjm1928246 <111906865+cjm1928246@users.noreply.github.com>
2022-08-23 14:33:32 -0400

    Update streamlit_app.py
    Added title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
+streamlit.title('Healthy Diner Menu')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
