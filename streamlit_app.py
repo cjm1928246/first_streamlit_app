3fb300c3a632b80f8062616d17edd0c1a59b0632
cjm1928246 <111906865+cjm1928246@users.noreply.github.com>
2022-08-24 14:27:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ try:
     streamlit.text(fruityvice_response.json())
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
-excpet URLError as e:
+except URLError as e:
   streamlit.error()
   
 streamlit.stop()
